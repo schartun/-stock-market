39d3deae31610638cfc526de93e4ab4572bbfed6
schartun <schartun@gmail.com>
2024-09-19 16:15:46 +0200

    Using Models for Cleaner Code

--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -1,31 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Stock } from '../../model/stock';
+
 @Component({
   selector: 'app-stock-item',
   templateUrl: './stock-item.component.html',
   styleUrl: './stock-item.component.css',
 })
 export class StockItemComponent implements OnInit {
-  public name: string;
-  public code: string;
-  public price: number;
-  public previousPrice: number;
-  public positiveChange: boolean;
-  public favorite: boolean;
+  public stock: Stock;
 
   constructor() {}
 
   ngOnInit(): void {
-    this.name = 'Test Stock Company';
-    this.code = 'TSC';
-    this.price = 85;
-    this.previousPrice = 8;
-    this.positiveChange = this.price >= this.previousPrice;
-    this.favorite = false;
+this.stock = new Stock('Test Stock Company', 'TSC', 85, 80);
   }
 
   toggleFavorite(event) {
     console.log('We are toggling the favorite state for this stock', event);
-    this.favorite = !this.favorite;
+   this.stock.favorite = !this.stock.favorite;
   }
 }
